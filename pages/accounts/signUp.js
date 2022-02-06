9f861da6de90fef7da1f17e23412bf1e1349db04
Alexander <ajtkalych@gmail.com>
2022-02-06 15:20:07 -0700

    init

--- pages/accounts/signUp.js
+++ pages/accounts/signUp.js
@@ -3,8 +3,6 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { ModalStyles } from 'styles/components/Modal';
 
-import CreateTransaction from 'components/multiple/forms/general/returnForm';
-
 // import initial state object
 import { AccountFields } from 'data/stateFields';
 
@@ -27,13 +25,6 @@ export default function CreateNewAccount() {
       isOpen // the modal should always be shown on page load, it is the 'page'
       onRequestClose={() => router.back()}
       contentLabel="Account modal"
-      style={ModalStyles}>
-      <CreateTransaction
-        type="Account"
-        stateFields={AccountFields}
-        mutationString={CREATE_NEW_ACCOUNT}
-        queryString={ALL_REQUESTS_QUERY}
-      />
-    </Modal>
+      style={ModalStyles}></Modal>
   );
 }
