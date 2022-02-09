355acdd6cf87985ac884164cccf0ff29a4ef8fef
Alexander <ajtkalych@gmail.com>
2022-02-09 14:34:44 -0700

    add api

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `https://api.ukrainianaid.com/api/graphql`;
-export const prodEndpoint = `https://ukrainianaid.com/api/graphql`;
+export const prodEndpoint = `https://api.ukrainianaid.com/api/graphql`;
 export const perPage = 4;
