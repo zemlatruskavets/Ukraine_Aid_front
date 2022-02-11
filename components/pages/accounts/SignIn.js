b5f4600b82865473b70679cc80c65d00a95ae16d
Alexander <ajtkalych@gmail.com>
2022-02-10 20:34:22 -0700

    fix styles

--- components/pages/accounts/SignIn.js
+++ components/pages/accounts/SignIn.js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 
 import { useMutation } from '@apollo/client';
-import { Form } from 'styles/components/Form';
+import { MainGrid, Form } from 'styles/components/Form';
 import useForm from 'lib/useForm';
 import Error from 'components/general/ErrorMessage';
 import { CURRENT_USER_QUERY } from 'graphQL/accounts/queries';
@@ -33,34 +33,34 @@ export default function SignIn() {
       ? data?.authenticatePersonWithPassword
       : undefined;
   return (
-    <Form method="POST" onSubmit={handleSubmit}>
-      <h2>Sign Into Your Account</h2>
-      <Error error={error} />
-      <fieldset>
-        <label htmlFor="email">
-          Email
-          <input
-            type="email"
-            name="email"
-            placeholder="Your Email Address"
-            autoComplete="email"
-            value={inputs.email}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="password">
-          Password
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            autoComplete="password"
-            value={inputs.password}
-            onChange={handleChange}
-          />
-        </label>
-        <button type="submit">Sign In!</button>
-      </fieldset>
-    </Form>
+    <MainGrid>
+      <Form method="POST" onSubmit={handleSubmit}>
+        <h2>Sign In</h2>
+        <Error error={error} />
+        <fieldset>
+          <label htmlFor="email">
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email Address"
+              autoComplete="email"
+              value={inputs.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="password">
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              autoComplete="password"
+              value={inputs.password}
+              onChange={handleChange}
+            />
+          </label>
+          <button type="submit">Sign In</button>
+        </fieldset>
+      </Form>
+    </MainGrid>
   );
 }
